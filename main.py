67d7492ba343538018e2ba1b14565e7a5a085d25
krishna1priya <krishnapriya051@gmail.com>
2024-10-13 23:24:18 -0400

    evaluation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from patra_agent.graph import run_patra_graph
 
 def main():
-    question = ("how many models are in the system?")
+    # few_shot_prompt = (
+    #     "If one model can be accessed by three users, then 6 users can access 2 models and If there are 4 models available,  12 users who can use all models. Based on this, what is the max number of users who can access all the models?"
+    # )
+    # question = ("One model can be used by three users. Based on this, What is the max number of users who can access all the models?")
+    question = ("Name the datasheet?")
     result = run_patra_graph(question)
     print(result)
 
